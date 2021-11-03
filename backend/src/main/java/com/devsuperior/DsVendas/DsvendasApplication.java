5bfe1980e324a8293b1150877053d62e4582eee6
Thor de Oliveira <thor.o.sonhador@gmail.com>
2021-11-03 19:45:11 -0300

    Project Create

---
+++ backend/src/main/java/com/devsuperior/DsVendas/DsvendasApplication.java
@@ -0,0 +1,14 @@
+package com.devsuperior.DsVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+		System.out.println("Vendo se isso resolve");
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/DsVendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
