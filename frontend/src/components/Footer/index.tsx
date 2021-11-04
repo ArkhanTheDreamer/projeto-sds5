a8b2d78e2c838244b372d28342cd58a80fc098da
Thor de Oliveira <thor.o.sonhador@gmail.com>
2021-11-03 23:51:13 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ArkhanTheDreamer" target="_blank" rel="noreferrer">Thor Oliveira</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/thorpinto" target="_blank" rel="noreferrer">@thorpinto</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
